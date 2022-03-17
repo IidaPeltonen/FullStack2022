e3117189f342f7e165eacef2638175afa79b1583
IidaPeltonen <n0peii00@students.oamk.fi>
2022-03-17 15:51:35 +0200

    step2 valmiiksi

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -11,17 +11,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(1) /*laskee kaikkien annettujen äänien kappale määrän*/
+  const [allClicksYht, setAllYht] = useState(0) /*laskee kaikkien annettujen äänien marvosanat yhteen*/
+  const [positiiviset, setPos] = useState(0) /*laskee positiivisten äänten kappalemäärän*/
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(allClicks + 1)
+    setAllYht(allClicksYht + 1)
+    setPos(positiiviset + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(allClicks + 1)
+    setAllYht(allClicksYht)
+    setPos(positiiviset)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(allClicks + 1)
+    setAllYht(allClicksYht - 1)
+    setPos(positiiviset)
   }
 
   return (
@@ -33,10 +45,12 @@ const App = () => {
       <h2>Statistiikka</h2>
       <p>Huippu: {good}</p>  
       <p>Neutraali: {neutral}</p>  
-      <p>Huono: {bad}</p>  
+      <p>Huono: {bad}</p> 
+      <p>Ääniä yhteensä: {(allClicks-1)} </p> 
+      <p>Äänien keskiarvo: {allClicksYht/(allClicks-1)}</p>
+      <p>Äänistä positiivisia: {positiiviset/(allClicks-1) * 100}  %</p>
     </div>
   )
 }
-
 
 export default App
