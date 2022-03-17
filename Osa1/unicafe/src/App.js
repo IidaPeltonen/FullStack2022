5dd63630478be79e65b84cd721d89e31fe287c01
IidaPeltonen <n0peii00@students.oamk.fi>
2022-03-17 14:48:03 +0200

    unicafe step1

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // muuttujat
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anna palautetta</h1>
+      {good}
+      <Button handleClick={handleGoodClick} text='Huippu!' />
+      {neutral}
+      <Button handleClick={handleNeutralClick} text='Neutraali' />
+      {bad}
+      <Button handleClick={handleBadClick} text='Huono!' />
+      <h2>Statistiikka</h2>
+      <p>Huippu: {good}</p>  
+      <p>Neutraali: {neutral}</p>  
+      <p>Huono: {bad}</p>  
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
