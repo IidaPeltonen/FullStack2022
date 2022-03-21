a144d9d0e6f6002d99c3edf79f2d9e48b1905b3e
IidaPeltonen <n0peii00@students.oamk.fi>
2022-03-21 12:46:31 +0200

    ok

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+/* Iida Peltonen 2022 */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+
+const Course = ({ props }) => {
+
+  const Header = (props) => {
+    return (
+    <div>
+      <h1>{props.course.name}</h1> 
+      console.log('props.course.name')
     </div>
-  );
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part
+          part1={props.parts.parts[0].name} /* osion nimi */
+          exercises1={props.parts.parts[0].exercises} /* osion tehtävät */
+        />
+        <Part
+          part2={props.parts.parts[1].name} /* osion nimi */
+          exercises2={props.parts.parts[1].exercises} /* osion tehtävät */
+        />
+        <Part
+          part3={props.parts.parts[2].name} /* osion nimi */
+          exercises3={props.parts.parts[0].exercises} /* osion tehtävät */
+        />
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part1} {props.exercises1} {/* yhdistää osion nimen ja tehtäviewn määrän */}
+        </p>
+        <p>
+          {props.part2} {props.exercises2}
+        </p>
+        <p>
+          {props.part3} {props.exercises3}
+        </p>
+      </div>
+    )
+  }
+ 
+/*   const Total = (props) => {
+    return (
+      <div>
+        {
+          <p>
+            Number of exercises{" "}
+            {props.parts.parts[0].exercises +
+              props.parts.parts[1].exercises +
+              props.parts.parts[2].exercises} 
+          </p>
+        }
+      </div>
+    )
+  }; */
+
 }
 
-export default App;
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
