10e297b898d004809f80cceb809a982ef5edb5d0
IidaPeltonen <n0peii00@students.oamk.fi>
2022-03-25 23:16:22 +0200

    puh

--- Osa2/puhelinluettelo/src/components/Person.js
+++ Osa2/puhelinluettelo/src/components/Person.js
@@ -1,10 +1,17 @@
 /* Iida Peltonen 2022 */
 
+
+
 const Person = ({ person }) => {
+
+  const Poista = ( id ) => {
+    //tähän tulee poisto
+  }
+  
   return (
     <li key={person.id}>
       {' '}
-      {person.name} {person.number}
+      {person.name} {person.number} <button onClick={Poista} id={person.id}>Poista</button>
     </li>
   )
 }
