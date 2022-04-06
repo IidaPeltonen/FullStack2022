30014a9e1a3c164167162c20ef852e8d4e161bbd
IidaPeltonen <n0peii00@students.oamk.fi>
2022-04-06 13:47:10 +0300

    vikat pikkufixit

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -2,11 +2,15 @@
 
 import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
-import UudenLisays from './components/UudenLisays'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = props => {
   const [persons, setPersons] = useState(props.persons) //kaikki tyypit
+  const [newPerson, setNewPerson] = useState('') //nimet
+  const [newNumber, setNewNumber] = useState('') //numerot
+  const [success, setSuccess] = useState('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
     personService
@@ -16,12 +20,86 @@ const App = props => {
       })
   }, [])
 
+    //uuden lisäys
+    const LisaaUusi = e => {
+      e.preventDefault()
+      const personObject = {
+        name: newPerson,
+        number: newNumber
+      }
+      let id
+      let samaNimi = false
+  
+      //käydään taulukon nimet läpi ja verrataan
+      persons.forEach((item, index) => {
+        //jos sama nimi löytyy
+        if (item.name.toLowerCase() === newPerson.toLowerCase()) {
+          samaNimi = true
+          // vaihdetaan "uuden" idksi vanhan id
+          id = item.id
+        }
+      })
+      if (samaNimi) {
+        let vastaus = window.confirm(`${newPerson} löytyy jo luettelosta, päivitetäänkö numero?`)
+        if (!vastaus) {
+          setNewPerson('')
+          setNewNumber('')
+        } else {
+          personService
+            .update(id, personObject)
+            .then((returnedPerson) => {
+              setPersons(persons.concat(returnedPerson))
+              setNewPerson('')
+              setNewNumber('')
+                setSuccess(`${returnedPerson.name} n numero päivitetty!`);
+                setTimeout(() => {
+                  setSuccess(null);
+                  window.location.reload(false);
+                }, 5000);
+              })
+            .catch((error) => {
+              setError(`${newPerson} n tiedot on jo poistettu`)
+              setTimeout(() => {
+                setError(null);
+                window.location.reload(false);
+              }, 5000);
+            })
+        }
+      } else {
+        personService.create(personObject).then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewPerson('')
+          setNewNumber('')
+          setSuccess(newPerson + 'n numero tallennettu')
+          setTimeout(() => {
+            setSuccess(null);
+            window.location.reload(false);
+          }, 5000);
+        })
+      }
+    }
+
+    const handlePersonChange = e => {
+      setNewPerson(e.target.value)
+    }
+  
+    const handleNumberChange = e => {
+      setNewNumber(e.target.value)
+    }
+
   return (
     <div>
       <h1>Puhelinluettelo</h1>
       <Filter persons={persons} />
       <h2>Tallenna uusi</h2>
-      <UudenLisays persons={persons} setPersons={setPersons} />
+      <form onSubmit={LisaaUusi}>
+        <Notification message={success} message2={error} />
+        Nimi: <input value={newPerson} onChange={handlePersonChange} />
+        <br />
+        Numero: <input value={newNumber} onChange={handleNumberChange} />
+        <br />
+        <button type='submit'>Tallenna</button>
+      </form>  
     </div>
   )
 }
