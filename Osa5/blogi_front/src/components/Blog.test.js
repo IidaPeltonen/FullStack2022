88e05962c8107e49378620deefeb65f760d09aa4
IidaPeltonen <n0peii00@students.oamk.fi>
2022-05-25 11:57:56 +0300

    5.15 valmis

--- Osa5/blogi_front/src/components/Blog.test.js
+++ Osa5/blogi_front/src/components/Blog.test.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -21,4 +22,26 @@ test('renders content', () => {
     const author = screen.getByText('Uusi')
     expect(author).toBeDefined()
 
+})
+
+test('clicking the like-button twice calls event handler twice', async () => {
+    const blog = {
+        title: 'Uusi',
+        author: 'Iida Peltonen',
+        url: 'www.iida.fi',
+        likes: 5,
+    }
+
+    const mockHandler = jest.fn()
+
+    render(
+        <Blog blog={blog} updateBlog={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('Like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
 })
