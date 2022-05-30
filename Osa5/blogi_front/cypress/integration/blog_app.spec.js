130ea0aa7c37c19d5f16b12db7941c67c704128a
IidaPeltonen <n0peii00@students.oamk.fi>
2022-05-30 14:23:25 +0300

    5.19 valmis

--- Osa5/blogi_front/cypress/integration/blog_app.spec.js
+++ Osa5/blogi_front/cypress/integration/blog_app.spec.js
@@ -1,40 +1,61 @@
-describe('Blog app', function() {
-    beforeEach(function() {
+describe('Blog app', function () {
+    beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
         //create user
         const user = {
-            name: 'Matti Luukkainen',
-            username: 'mluukkai',
-            password: 'salainen'
+            name: 'Iida Peltonen',
+            username: 'Iida',
+            password: 'salasana'
         }
         cy.request('POST', 'http://localhost:3000/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
-    it('Login form is shown', function() {
+    it('Login form is shown', function () {
         cy.visit('http://localhost:3000')
         cy.contains('BLOGS')
         cy.contains('Username')
     })
-    describe('Login',function() {
-        it.only('succeeds with correct credentials', function() {
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
             cy.contains('Login').click()
-            cy.get('#Username').type('mluukkai')
-            cy.get('#Password').type('salainen')
+            cy.get('#Username').type('Iida')
+            cy.get('#Password').type('salasana')
             cy.get('#login').click()
 
-            cy.contains('Logged in as mluukkai')
+            cy.contains('Logged in as Iida')
         })
 
-        it('fails with wrong credentials', function() {
+        it('fails with wrong credentials', function () {
             cy.contains('Login').click()
-            cy.get('#Username').type('mluukkai')
+            cy.get('#Username').type('Iida')
             cy.get('#Password').type('wrong')
             cy.get('#login').click()
 
             cy.contains('Wrong credentials')
         })
     })
+    describe('When logged in', function () {
+        beforeEach(function () {
+            // log in user here
+            cy.contains('Login').click()
+            cy.get('#Username').type('Iida')
+            cy.get('#Password').type('salasana')
+            cy.get('#login').click()
+
+            cy.contains('Logged in as Iida')
+        })
+
+        it('A blog can be created', function () {
+            cy.contains('Add new blog').click()
+            cy.get('#title').type('Cypress-testi')
+            cy.get('#author').type('Iida Peltonen')
+            cy.get('#url').type('www.cypress-testi.com')
+            cy.get('#likes').type('2')
+            cy.get('#submit').click()
+
+            cy.contains('New blog Cypress-testi added')
+            cy.contains('Cypress-testi')
+        })
+    })
 })
-
-
--- Osa5/blogi_front/src/components/BlogForm.js
+++ Osa5/blogi_front/src/components/BlogForm.js
@@ -55,7 +55,7 @@ const BlogForm = ({ addBlog }) => {
                 />
                 <br />
                 <br />
-                <button type="submit">Create</button>
+                <button id='submit' type="submit">Create</button>
             </form>
         </div>
     )
@@ -63,63 +63,3 @@ const BlogForm = ({ addBlog }) => {
 
 export default BlogForm
 
-
-/*uuden lisäys
-const BlogForm = ({
-    addBlog,
-    newTitle,
-    newAuthor,
-    newUrl,
-    newLikes,
-    handleTitleChange,
-    handleAuthorChange,
-    handleUrlChange,
-    handleLikesChange
-}) => {
-
-    return(
-        <div>
-            <h1>Add new blog</h1>
-            <form onSubmit={addBlog}>
-                <label htmlFor="title">Title </label>
-                <input
-                    id="title"
-                    name="title"
-                    value={newTitle}
-                    onChange={handleTitleChange}
-                    placeholder='Title'
-                /><br></br>
-                <label htmlFor="author">Author </label>
-                <input
-                    id="author"
-                    name="author"
-                    value={newAuthor}
-                    onChange={handleAuthorChange}
-                    placeholder='Author'
-                /><br></br>
-                <label htmlFor='url'>Url </label>
-                <input
-                    id="url"
-                    name="url"
-                    value={newUrl}
-                    onChange={handleUrlChange}
-                    placeholder='Url'
-                /><br></br>
-                <label htmlFor='url'>Likes </label>
-                <input
-                    id="likes"
-                    name="likes"
-                    type="number"
-                    value={newLikes}
-                    onChange={handleLikesChange}
-                    placeholder='Likes'
-                />
-                <br />
-                <br />
-                <button type="submit">Create</button>
-            </form>
-        </div>
-    )
-}
-
-export default BlogForm */
